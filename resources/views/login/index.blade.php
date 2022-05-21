92888c726e4521f9766de6441194fd8ea3123fd5
MuhammadAliFikri019 <muhammadalifikri948@gmail.com>
2022-05-21 17:32:51 +0700

    Update index.blade.php

--- resources/views/login/index.blade.php
+++ resources/views/login/index.blade.php
@@ -21,7 +21,7 @@
 
 
   <main class="form-signin my-5 py-5">
-    <h1 class="h3 mb-3 fw-normal text-center">Please Login</h1>
+    <h1 class="h3 mb-3 fw-normal text-center">LOGIN SIAKAD</h1>
     <form action="/login" method="post">
       @csrf
       <div class="form-floating">
