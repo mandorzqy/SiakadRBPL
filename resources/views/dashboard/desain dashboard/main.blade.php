547eeecd96a6795e4d8e84a9b62d0bfa85d0c5d7
ReshaRamzy <ramzyarkansa@gmail.com>
2022-05-24 21:19:38 +0700

    edit css

--- resources/views/dashboard/desain dashboard/main.blade.php
+++ resources/views/dashboard/desain dashboard/main.blade.php
@@ -13,11 +13,11 @@
   <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
 
   <!-- Vendor CSS Files -->
-  <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-  <link href="vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
-  <link href="vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
-  <link href="vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
-  <link href="vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+  <link href="public/css/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="public/css/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+  <link href="public/css/boxicons/css/boxicons.min.css" rel="stylesheet">
+  <link href="public/css/glightbox/css/glightbox.min.css" rel="stylesheet">
+  <link href="public/css/swiper/swiper-bundle.min.css" rel="stylesheet">
 
   <!-- Template Main CSS File -->
   <link href="public/css/dashboard.css" rel="stylesheet">
