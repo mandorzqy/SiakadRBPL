a1c73493cec756148d45535566cfedafef7e71a0
MirzaBanana <badarudinmirza@gmail.com>
2022-06-05 20:50:24 +0700

    pembuatan laman kuesioner

---
+++ resources/views/proses/kuesioner/index.blade.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Kuesioner Dosen dan Mata Kuliah</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+</head>
+<body>
+<center>
+  <h2>Kuesioner Evaluasi Mata Kuliah</h2>
+  <h4>Riwayat Pengisian Kuesioner</h4>
+  <p></p>
+  <div class="btn-group">
+    <button type="button" class="btn btn-outline-secondary dropdown-toggle" data-bs-toggle="dropdown">Periode</button>
+      <ul class="dropdown-menu">
+        <li><a class="dropdown-item" href="#">Genap</a></li>
+        <li><a class="dropdown-item" href="#">Ganjil</a></li>
+      </ul>
+      <button type="button" class="btn btn-outline-secondary dropdown-toggle" data-bs-toggle="dropdown">Tahun</button>
+      <ul class="dropdown-menu">
+        <li><a class="dropdown-item" href="#">2022</a></li>
+        <li><a class="dropdown-item" href="#">2021</a></li>
+        <li><a class="dropdown-item" href="#">2020</a></li>
+      </ul>
+    <button type="button" class="btn btn-outline-secondary">Enter</button>
+  </div>
+  <p>Data update setiap satu jam</p>
+  <table class="table">
+    <thead>
+      <tr>
+        <th>No.</th>
+        <th>Kode MK</th>
+        <th>Dosen</th>
+        <th>Status</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td>1</td>
+        <td>IS184412</td>
+        <td>Radityo Prasetianto Wibowo, S.Kom., M.Kom</td>
+        <td>Belum</td>
+      </tr>
+    </tbody>
+  </table>
+</div>
+
+</body>
+</center>
+</html>
