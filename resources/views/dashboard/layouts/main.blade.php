b9e737387164e241162dc4a298aa395a3fb1801a
dehanfrhn <rahmandika.205026@mhs.its.ac.id>
2022-06-07 12:58:29 +0700

    routing ikoma

--- resources/views/dashboard/layouts/main.blade.php
+++ resources/views/dashboard/layouts/main.blade.php
@@ -40,7 +40,7 @@
     -@include('dashboard.layouts.header')
     <div class="container-fluid">
     <div class="row">
-        @include('dashboard.layouts.sidebar')
+        {{-- @include('dashboard.layouts.sidebar') --}}
         <main class="col-md-9">
         @yield('container')
         </div>
