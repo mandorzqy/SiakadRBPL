e3e566a9676b5a19aaf2ec0e1db057ea67607c61
girhan <lusandi.girhan@gmail.com>
2022-06-09 16:18:19 +0700

    desain dashboard untuk role admin

--- resources/views/admin/adminDashboard/index.blade.php
+++ resources/views/admin/adminDashboard/index.blade.php
@@ -1,8 +1,112 @@
 @extends('dashboard.layouts.main')
 
 @section('container')
-<div class="d-flex text-center justify-content-center">
-    <h1 class="h2 text-center mt-5">Welcome back, Admin</h1>
-    <div class="btn-toolbar mb-2 mb-md-0">
-</div>
+ <!-- Vendor CSS Files -->
+ <link href="css/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+ <link href="css/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+ <link href="css/boxicons/css/boxicons.min.css" rel="stylesheet">
+ <link href="css/glightbox/css/glightbox.min.css" rel="stylesheet">
+ <link href="css/swiper/swiper-bundle.min.css" rel="stylesheet">
+
+ <!-- Template Main CSS File -->
+ <link href="css/dashboard.css" rel="stylesheet">
+<style>
+ body {
+ background-image: url("https://media.istockphoto.com/photos/abstract-blue-cyan-background-picture-id995719694?k=20&m=995719694&s=612x612&w=0&h=zI2SFqWdZallvqgVphnq5T8ZWYK0TBIkKdyYP60JAI8=");
+}
+
+</style>
+</head>
+
+<body>
+   <!-- ======= Portfolio Section ======= -->
+   <section id="portfolio" class="portfolio">
+
+
+     <div class="container">
+       <div class="container pt-5">
+         <div class="section-title">
+           <h2>Dashboard</h2>
+           <p>Selamat Datang, {{auth()->user()->name}}</p>
+       </div>
+       </div>
+
+       <div class="row portfolio-container">
+
+         <div class="col-lg-4 col-md-6 portfolio-item filter-app wow fadeInUp">
+           <div class="portfolio-wrap">
+             <figure>
+               <img src="img/portfolio-1.jpg" class="img-fluid" alt="">
+               <i class="bookmarks"></i>
+               <a href="img/portfolio-1.jpg" data-gallery="portfolioGallery" class="link-preview portfolio-lightbox" title="Preview"><i class="bx bx-plus"></i></a>
+               <a href="portfolio-details.html" class="link-details" title="More Details"><i class="bx bx-link"></i></a>
+             </figure>
+
+             <div class="portfolio-info">
+               <h4><a href="/admin/biodata">Biodata</a></h4>
+             </div>
+           </div>
+         </div>
+
+         <div class="col-lg-4 col-md-6 portfolio-item filter-web wow fadeInUp" data-wow-delay="0.1s">
+           <div class="portfolio-wrap">
+             <figure>
+               <img src="img/portfolio-02.jpg" class="img-fluid" alt="">
+               <a href="img/portfolio-02.jpg" class="link-preview portfolio-lightbox" data-gallery="portfolioGallery" title="Preview"><i class="bx bx-plus"></i></a>
+               <a href="portfolio-details.html" class="link-details" title="More Details"><i class="bx bx-link"></i></a>
+             </figure>
+
+             <div class="portfolio-info">
+               <h4><a href="/admin/transkrip">Transkrip</a></h4>
+             </div>
+           </div>
+         </div>
+
+         <div class="col-lg-4 col-md-6 portfolio-item filter-app wow fadeInUp" data-wow-delay="0.2s">
+           <div class="portfolio-wrap" >
+             <figure>
+               <img src="img/portfolio-03.jpg" class="img-fluid" alt="">
+               <i class="bi bi-bank"></i>
+               <a href="img/portfolio-03.jpg" class="link-preview portfolio-lightbox" data-gallery="portfolioGallery" title="Preview"><i class="bx bx-plus"></i></a>
+             </figure>
+
+             <div class="portfolio-info">
+               <h4><a href="/admin-kurikulum">Daftar Mata Kuliah</a></h4>
+             </div>
+           </div>
+         </div>
+
+         <div class="col-lg-4 col-md-6 portfolio-item filter-app wow fadeInUp" data-wow-delay="0.2s">
+            <div class="portfolio-wrap" >
+              <figure>
+                <img src="img/portfolio-03.jpg" class="img-fluid" alt="">
+                <i class="bi bi-bank"></i>
+                <a href="img/portfolio-03.jpg" class="link-preview portfolio-lightbox" data-gallery="portfolioGallery" title="Preview"><i class="bx bx-plus"></i></a>
+              </figure>
+
+              <div class="portfolio-info">
+                <h4><a href="/admin/jadwal">Jadwal Kuliah Mahasiswa</a></h4>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-lg-4 col-md-6 portfolio-item filter-app wow fadeInUp" data-wow-delay="0.2s">
+            <div class="portfolio-wrap" >
+              <figure>
+                <img src="img/portfolio-03.jpg" class="img-fluid" alt="">
+                <i class="bi bi-bank"></i>
+                <a href="img/portfolio-03.jpg" class="link-preview portfolio-lightbox" data-gallery="portfolioGallery" title="Preview"><i class="bx bx-plus"></i></a>
+              </figure>
+
+              <div class="portfolio-info">
+                <h4><a href="/admin/kelas">Kelas</a></h4>
+              </div>
+            </div>
+          </div>
+       </div>
+
+     </div>
+
+   </section><!-- End Portfolio Section -->
+</body>
 @endsection
