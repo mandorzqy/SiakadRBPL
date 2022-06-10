1acd0d06535ad2bac93aa162037db6bbc59fe3d6
MuhammadAliFikri019 <muhammadalifikri948@gmail.com>
2022-06-10 08:58:53 +0700

    Cetak surat
    Penambahan cetak

---
+++ resources/views/suratmahasiswa/suratundurdiri/cetak.blade.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+  <title>Cetak Surat</title>
+  <meta content="" name="description">
+  <meta content="" name="keywords">
+
+  <!-- Google Fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+
+  <!-- Vendor CSS Files -->
+  <link href="css/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="css/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+  <link href="css/boxicons/css/boxicons.min.css" rel="stylesheet">
+  <link href="css/glightbox/css/glightbox.min.css" rel="stylesheet">
+  <link href="css/swiper/swiper-bundle.min.css" rel="stylesheet">
+</head>
+
+<body>
+
+  <!-- ======= Header ======= -->
+  
+    <!-- ======= Portfolio Section ======= -->
+
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>praktikum</title>
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+        <script src="https://cdn.jsdelivr.net/npm/jquery-validation@1.19.3/dist/jquery.validate.js"></script>
+        <script src="https://cdn.jsdelivr.net/npm/jquery-validation@1.19.3/dist/additional-methods.min.js"></script>
+
+        <style>
+            .card {
+                width: 70%;
+                border-radius: 15px;
+            }
+            .card-header{
+                text-align: center;
+                border: 0;
+                padding-top: 120px;
+                padding-bottom: 40px;
+            }
+        </style>
+
+    </head>
+
+    <body>
+        <div class="container">
+          <div class="card mx-auto">
+            <div class="card-header">
+                <h2><b>Cetak Surat</b></h2>
+            </div>
+            
+        </div>
+        </div>
+
+    </body>
+    </section><!-- End Portfolio Section -->
+</body>
