ccd768f11281c187e33daf4c11317d9ed4ef178a
MUHAMMAD ARMANDO NUR RIZQY ANSAR <87767318+armando2326@users.noreply.github.com>
2022-06-11 11:25:56 +0700

    update on view dashboard

--- resources/views/dashboard/desaindashboard/dashboard.blade.php
+++ resources/views/dashboard/desaindashboard/dashboard.blade.php
@@ -2,42 +2,38 @@
 <html lang="en">
 
 <head>
-  <meta charset="utf-8">
-  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+    <meta charset="utf-8">
+    <meta content="width=device-width, initial-scale=1.0" name="viewport">
 
-  <title>SIAKAD</title>
-  <meta content="" name="description">
-  <meta content="" name="keywords">
+    <title>SIAKAD</title>
+    <meta content="" name="description">
+    <meta content="" name="keywords">
 
-  <!-- Google Fonts -->
-  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+    <!-- Google Fonts -->
+    <link
+        href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i"
+        rel="stylesheet">
 
-  <!-- Vendor CSS Files -->
-  <link href="css/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-  <link href="css/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
-  <link href="css/boxicons/css/boxicons.min.css" rel="stylesheet">
-  <link href="css/glightbox/css/glightbox.min.css" rel="stylesheet">
-  <link href="css/swiper/swiper-bundle.min.css" rel="stylesheet">
+    <!-- Vendor CSS Files -->
+    <link href="css/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+    <link href="css/boxicons/css/boxicons.min.css" rel="stylesheet">
+    <link href="css/glightbox/css/glightbox.min.css" rel="stylesheet">
+    <link href="css/swiper/swiper-bundle.min.css" rel="stylesheet">
 
-  <!-- Template Main CSS File -->
-  <link href="css/dashboard.css" rel="stylesheet">
-<style>
-<<<<<<< Updated upstream
- 
- 
-=======
-  body {
-    background-color:white;
-  }
->>>>>>> Stashed changes
-
-</style>
+    <!-- Template Main CSS File -->
+    <link href="css/dashboard.css" rel="stylesheet">
+    <style>
+        body {
+            background-color: white;
+        }
+    </style>
 </head>
 
 <body>
 
-  <!-- ======= Header ======= -->
-  {{-- <header id="header" class="fixed-top d-flex align-items-center">
+    <!-- ======= Header ======= -->
+    {{-- <header id="header" class="fixed-top d-flex align-items-center">
     <div class="container d-flex align-items-center">
 
       <div class="logo me-auto">
@@ -62,110 +58,108 @@
       </nav><!-- .navbar -->
 
   </header><!-- End Header --> --}}
-  @include('dashboard.layouts.header')
+    @include('dashboard.layouts.header')
     <!-- ======= Portfolio Section ======= -->
     <section id="portfolio" class="portfolio">
 
 
-      <div class="container">
-        <div class="container pt-5">
-          <div class="section-title">
-            <h2>Dashboard</h2>
-            <p>Selamat Datang, {{auth()->user()->name}}</p>
+        <div class="container">
+            <div class="container pt-5">
+                <div class="section-title">
+                    <h2>Dashboard</h2>
+                    <p>Selamat Datang, {{ auth()->user()->name }}</p>
+                </div>
+            </div>
+
+            <div class="row portfolio-container">
+
+                <div class="col-lg-4 col-md-6 portfolio-item filter-app wow fadeInUp">
+                    <div class="portfolio-wrap">
+                        <a href="/proses">
+                            <figure>
+                                <img src="img/portfolio-1.jpg" class="img-fluid" alt="">
+                            </figure>
+
+                            <div class="portfolio-info">
+                                <h4>Proses</h4>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+
+                <div class="col-lg-4 col-md-6 portfolio-item filter-web wow fadeInUp" data-wow-delay="0.1s">
+                    <div class="portfolio-wrap">
+                        <a href="/suratmahasiswa">
+                            <figure>
+                                <img src="img/portfolio-02.jpg" class="img-fluid" alt="">
+                            </figure>
+
+                            <div class="portfolio-info">
+                                <h4>Surat Mahasiswa</h4>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+
+                <div class="col-lg-4 col-md-6 portfolio-item filter-app wow fadeInUp" data-wow-delay="0.2s">
+                    <div class="portfolio-wrap">
+                        <a href="/biayapendidikan">
+                          <figure>
+                              <img src="img/portfolio-03.jpg" class="img-fluid" alt="">
+                          </figure>
+
+                          <div class="portfolio-info">
+                              <h4>Biaya Pendidikan</h4>
+                          </div>
+                      </a>
+                    </div>
+                </div>
+
+                <div class="col-lg-4 col-md-6 portfolio-item filter-card wow fadeInUp">
+                    <div class="portfolio-wrap">
+                        <a href="/akademik">
+                          <figure>
+                              <img src="img/portfolio-04.jpg" class="img-fluid" alt="">
+                          </figure>
+
+                          <div class="portfolio-info">
+                              <h4>Akademik</h4>
+                          </div>
+                      </a>
+                    </div>
+                </div>
+
+                <div class="col-lg-4 col-md-6 portfolio-item filter-web wow fadeInUp" data-wow-delay="0.1s">
+                    <div class="portfolio-wrap">
+                       <a href="/akademik">
+                          <figure>
+                              <img src="img/portfolio-05.jpg" class="img-fluid" alt="">
+                          </figure>
+
+                          <div class="portfolio-info">
+                              <h4>Laporan</h4>
+                          </div>
+                      </a>
+                    </div>
+                </div>
+
+                <div class="col-lg-4 col-md-6 portfolio-item filter-app wow fadeInUp" data-wow-delay="0.2s">
+                    <div class="portfolio-wrap">
+                        <a href="/yudisium">
+                          <figure>
+                              <img src="img/portfolio-05.jpg" class="img-fluid" alt="">
+                          </figure>
+
+                          <div class="portfolio-info">
+                              <h4>Yudisium</h4>
+                          </div>
+                      </a>
+                    </div>
+                </div>
+
+            </div>
+
         </div>
-        </div>
-
-        <div class="row portfolio-container">
-
-          <div class="col-lg-4 col-md-6 portfolio-item filter-app wow fadeInUp">
-            <div class="portfolio-wrap">
-              <figure>
-                <img src="img/portfolio-1.jpg" class="img-fluid" alt="">
-                <i class="bookmarks"></i>
-                <a href="img/portfolio-1.jpg" data-gallery="portfolioGallery" class="link-preview portfolio-lightbox" title="Preview"><i class="bx bx-plus"></i></a>
-                <a href="portfolio-details.html" class="link-details" title="More Details"><i class="bx bx-link"></i></a>
-              </figure>
-
-              <div class="portfolio-info">
-                <h4><a href="/proses">Proses</a></h4>
-              </div>
-            </div>
-          </div>
-
-          <div class="col-lg-4 col-md-6 portfolio-item filter-web wow fadeInUp" data-wow-delay="0.1s">
-            <div class="portfolio-wrap">
-              <figure>
-                <img src="img/portfolio-02.jpg" class="img-fluid" alt="">
-                <a href="img/portfolio-02.jpg" class="link-preview portfolio-lightbox" data-gallery="portfolioGallery" title="Preview"><i class="bx bx-plus"></i></a>
-                <a href="portfolio-details.html" class="link-details" title="More Details"><i class="bx bx-link"></i></a>
-              </figure>
-
-              <div class="portfolio-info">
-                <h4><a href="/suratmahasiswa">Surat Mahasiswa</a></h4>
-              </div>
-            </div>
-          </div>
-
-          <div class="col-lg-4 col-md-6 portfolio-item filter-app wow fadeInUp" data-wow-delay="0.2s">
-            <div class="portfolio-wrap" >
-              <figure>
-                <img src="img/portfolio-03.jpg" class="img-fluid" alt="">
-                <i class="bi bi-bank"></i>
-                <a href="img/portfolio-03.jpg" class="link-preview portfolio-lightbox" data-gallery="portfolioGallery" title="Preview"><i class="bx bx-plus"></i></a>
-                <a href="/suratmahasiswa">{{auth()->user()->name}} class="link-details" title="More Details"><i class="bx bx-link"></i></a>
-              </figure>
-
-              <div class="portfolio-info">
-                <h4><a href="/biayapendidikan">Biaya Pendidikan</a></h4>
-              </div>
-            </div>
-          </div>
-
-          <div class="col-lg-4 col-md-6 portfolio-item filter-card wow fadeInUp">
-            <div class="portfolio-wrap">
-              <figure>
-                <img src="img/portfolio-04.jpg" class="img-fluid" alt="">
-                <a href="img/portfolio-04.jpg" class="link-preview portfolio-lightbox" data-gallery="portfolioGallery" title="Preview"><i class="bx bx-plus"></i></a>
-                <a href="portfolio-details.html" class="link-details" title="More Details"><i class="bx bx-link"></i></a>
-              </figure>
-
-              <div class="portfolio-info">
-                <h4><a href="/akademik">Akademik</a></h4>
-              </div>
-            </div>
-          </div>
-
-          <div class="col-lg-4 col-md-6 portfolio-item filter-web wow fadeInUp" data-wow-delay="0.1s">
-            <div class="portfolio-wrap">
-              <figure>
-                <img src="img/portfolio-05.jpg" class="img-fluid" alt="">
-                <a href="img/portfolio-05.jpg" class="link-preview portfolio-lightbox" data-gallery="portfolioGallery" title="Preview"><i class="bx bx-plus"></i></a>
-                <a href="portfolio-details.html" class="link-details" title="More Details"><i class="bx bx-link"></i></a>
-              </figure>
-
-              <div class="portfolio-info">
-                <h4><a href="/laporan">Laporan</a></h4>
-              </div>
-            </div>
-          </div>
-
-          <div class="col-lg-4 col-md-6 portfolio-item filter-app wow fadeInUp" data-wow-delay="0.2s">
-            <div class="portfolio-wrap">
-              <figure>
-                <img src="img/portfolio-06.jpg" class="img-fluid" alt="">
-                <a href="img/portfolio-06.jpg" class="link-preview portfolio-lightbox" data-gallery="portfolioGallery" title="Preview"><i class="bx bx-plus"></i></a>
-                <a href="portfolio-details.html" class="link-details" title="More Details"><i class="bx bx-link"></i></a>
-              </figure>
-
-              <div class="portfolio-info">
-                <h4><a href="/yudisium">Yudisium</a></h4>
-              </div>
-            </div>
-          </div>
-
-        </div>
-
-      </div>
 
     </section><!-- End Portfolio Section -->
 </body>
